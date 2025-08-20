beed7c6b0ffdf4cb372884ce0666d205ccba8c3b
Vinicius Duarte <viniciusduarte@outlook.com>
2025-08-20 01:25:07 +0100

    add telegram bot

--- src/components/email-form-header/email-form-header.tsx
+++ src/components/email-form-header/email-form-header.tsx
@@ -2,20 +2,23 @@ import styles from "@/components/email-form-header/email-form-header.module.css"
 import { redirect } from "next/navigation";
 import { SendEmail } from "@/services/send-email";
 import { SubmitButton } from "@/components/submit-button/submit-button";
+import { SendTelegramMessage } from '@/services/send-message-telegram';
 
 export const EmailFormHeader = () => {
   async function handleSendEmail(formData: FormData) {
     "use server";
 
-    const telephone = formData.get("telephone");
-
-    await SendEmail({
-      telephone: String(telephone),
-    });
-
-    formData.set("", "");
-
-    redirect("/obrigado");
+    const name = formData.get("name");
+        const telephone = formData.get("telephone");
+    
+        await SendTelegramMessage({
+          name: String(name),
+          telephone: String(telephone),
+        });
+    
+        formData.set("", "");
+    
+        redirect("/obrigado");
   }
 
   return (
--- src/util/telegramURIFormater.ts
+++ src/util/telegramURIFormater.ts
@@ -3,12 +3,11 @@ export const telegramURIFormater = ({
   name,
   telephone,
 }: {name: string, telephone: string}): string => {
-  const chatID = process.env.NEXT_PUBLIC_CHAT_ID;
-  const telegramToken = process.env.NEXT_PUBLIC_TELEGRAM_BOT_TOKEN;
+  const chatID = process.env.CHAT_ID;
+  const telegramToken = process.env.TELEGRAM_BOT_TOKEN;
 
-  const url = 'asdjaslj'
   const message = encodeURIComponent(
-    `<b>Novo Cliente</b>\nOrigen: ${url}\n\n<b>Nome:</b> ${name}\n<b>Telemóvel:</b> ${telephone}`
+    `<b>Novo Cliente</b>\n\n<b>Nome:</b> ${name}\n<b>Telemóvel:</b> ${telephone}`
   );
   const uri = `https://api.telegram.org/bot${telegramToken}/sendMessage?chat_id=${chatID}&text=${message}&parse_mode=html`;
 
